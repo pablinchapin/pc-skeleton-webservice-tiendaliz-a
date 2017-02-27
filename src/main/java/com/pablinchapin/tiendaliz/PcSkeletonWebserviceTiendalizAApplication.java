cd77bd68e6a446ce97beb90a4ff2ac5ea3eac8e5
pablinchapin <pablinchapin@gmail.com>
2017-02-27 08:49:43 -0600

    Iniciando el repositorio

---
+++ src/main/java/com/pablinchapin/tiendaliz/PcSkeletonWebserviceTiendalizAApplication.java
@@ -0,0 +1,12 @@
+package com.pablinchapin.tiendaliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcSkeletonWebserviceTiendalizAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcSkeletonWebserviceTiendalizAApplication.class, args);
+	}
+}
